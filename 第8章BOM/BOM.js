a31a82b29026019ca24a2af28f1cdc78ec5a9188
LzhElite <596553783@qq.com>
2015-08-30 14:15:38 +0800

    add 8

---
+++ 第8章BOM/BOM.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Elite on 2015/8/29.
+ */
+/*使用下列代码可以跨浏览器取得窗口左边和上边的位置*/
+var leftPos=(typeof window.screenLeft=="number")?window.screenLeft:window.screenX;
+var topPos=(typeof window.screenLeft=="number")?window.screenLeft:window.screenX;
+
+
+/*视口（viewport）*/
+var pageWidth=window.innerWidth,
+    pageHeight=window.innerHeight;
+if(typeof pageWidth!="number"){
+    if(document.compatMode=="CSS1Compat"){ /*是否是标准模式*/
+        pageWidth=document.documentElement.clientWidth;
+        pageHeight=document.documentElement.clientHeight;
+    }else{
+        pageWidth=document.body.clientWidth;
+        pageHeight=document.body.clientHeight;
+    }
+}
